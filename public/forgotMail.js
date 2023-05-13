bc12d632d1e9dcad28bf2bbb313e1b23cc239695
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-05-14 01:48:28 +0530

    dep

---
+++ public/forgotMail.js
@@ -0,0 +1,17 @@
+document.getElementById('forgotpwd').addEventListener('click', forgotPwd);
+
+function forgotPwd(e) {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  console.log(email);
+  axios
+    .post('http://54.89.86.238:4000/forgotpassword', {
+      email: email,
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
---
+++ public/signUp.js
@@ -0,0 +1,15 @@
+const signUp = (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  const userDetails = { name, email, password };
+
+  axios
+    .post('http://54.89.86.238:3000/user/add-user', userDetails)
+    .then((res) => {
+      window.alert('you are registered successfully!');
+      window.location.href = 'http://127.0.0.1:5501/logIn.html';
+    })
+    .catch((err) => console.log(err));
+};
