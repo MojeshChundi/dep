05508ca3eeda750baf166e96e045b4b11266d09b
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-06-12 13:22:01 +0530

    changed

--- public/forgotMail.js
+++ public/forgotMail.js
@@ -3,12 +3,12 @@ document.getElementById("forgotpwd").addEventListener("click", forgotPwd);
 function forgotPwd(e) {
   e.preventDefault();
   const email = document.getElementById("email").value;
-  //console.log(email);
+  console.log(email);
+  window.alert("mail sent succesfully!");
   axios
     .post("http://18.209.24.196:3000/pwd", { email: email })
     .then((res) => {
       console.log(res);
-      window.alert("mail sent succesfully!");
     })
     .catch((err) => {
       console.log(err);
